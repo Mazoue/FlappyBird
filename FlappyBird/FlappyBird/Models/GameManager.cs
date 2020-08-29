89611b6cc6d0521b9b9b33e6705464dcbae577d3
Mazoue <randomcomms+github@outlook.com>
2020-08-29 20:44:56 +1200

    Implemented gravity and state redraw

--- FlappyBird/FlappyBird/Models/GameManager.cs
+++ FlappyBird/FlappyBird/Models/GameManager.cs
@@ -1,27 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FlappyBird.Models
 {
-    public class GameManager
+    public class GameManager : INotifyPropertyChanged
     {
-        public BirdModel Bird { get; private set; }
         private readonly int _gravity = 2;
+        public bool IsRunning { get; private set; } = false;
 
-        public async void MainLoop()
-        {
-            while (true)
-            {
-                Bird.Fall(_gravity);
-                await Task.Delay(20); // wait 20ms
-            }
-        }
-
+        public event PropertyChangedEventHandler PropertyChanged;
+        public BirdModel Bird { get; private set; }
         public GameManager()
         {
             Bird = new BirdModel();
+        }
+        public async void MainLoop()
+        {
+            IsRunning = true;
+            while (IsRunning)
+            {
+                Bird.Fall(_gravity);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Bird)));
+                if (Bird.DistanceFromBottom <= 0)
+                    GameOver();
+                await Task.Delay(20);
+            }
+        }
+
+        private void GameOver()
+        {
+            IsRunning = false;
         }
     }
 }
