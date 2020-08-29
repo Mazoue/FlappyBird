20874d11f8ccb6f6f56ea91ff899490f4eab4026
Mazoue <warren.mazoue@tauranga.govt.nz>
2020-08-29 21:34:36 +1200

    Added jump

--- FlappyBird/FlappyBird/Models/BirdModel.cs
+++ FlappyBird/FlappyBird/Models/BirdModel.cs
@@ -8,10 +8,17 @@ namespace FlappyBird.Models
     public class BirdModel
     {
         public int DistanceFromBottom { get; private set; } = 100;
+        public int JumpStrength { get; private set; } = 50;
 
         public void Fall(int gravity)
         {
             DistanceFromBottom -= gravity;
+        }
+
+        public void Jump()
+        {
+            if (DistanceFromBottom < 530)
+                DistanceFromBottom += JumpStrength;
         }
     }
 }
