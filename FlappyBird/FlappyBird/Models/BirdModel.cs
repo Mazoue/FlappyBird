9fb8a34657ac4f9ed99baca6399958e70eeb30be
Mazoue <randomcomms+github@outlook.com>
2020-08-29 19:55:16 +1200

    Added  bird model(with gravity) and manager classes.

---
+++ FlappyBird/FlappyBird/Models/BirdModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlappyBird.Models
+{
+    public class BirdModel
+    {
+        public int DistanceFromBottom { get; private set; } = 100;
+
+        public void Fall(int gravity)
+        {
+            DistanceFromBottom -= gravity;
+        }
+    }
+}
---
+++ FlappyBird/FlappyBird/Models/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlappyBird.Models
+{
+    public class GameManager
+    {
+        public BirdModel Bird { get; private set; }
+        private readonly int _gravity = 2;
+
+        public async void MainLoop()
+        {
+            while (true)
+            {
+                Bird.Fall(_gravity);
+                await Task.Delay(20); // wait 20ms
+            }
+        }
+
+        public GameManager()
+        {
+            Bird = new BirdModel();
+        }
+    }
+}
